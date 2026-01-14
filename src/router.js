9ca5c852c69e39b18c892c8a1a2cdd90122b58c1
yevst <evstratovsergey@gmail.com>
2026-01-14 19:27:30 +0200

    Fix router history for GitHub Pages (hash mode)

--- src/router.js
+++ src/router.js
@@ -1,12 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/Home.vue'
 import About from './views/About.vue'
 import Auth from './views/Auth.vue'
 import Info from './views/Info.vue'
 
 const router = createRouter({
-  history: createWebHistory('/calorie-calculator-vue/'),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', component: Home },
     { path: '/about', component: About },
@@ -16,3 +15,4 @@ const router = createRouter({
 })
 
 export default router
+
